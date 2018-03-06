2c0fb0a8041d8c18bb3ac03e6948dc5c1b5506a4
James Gillman <jronaldgillman@gmail.com>
2018-03-05 21:36:35 -0700

    Proper Update to P3

--- LinkList/LinkList.java
+++ LinkList/LinkList.java
@@ -1,14 +1,45 @@
-package LinkList;
+//James Gillman
+//3/5/2018
+//Dominion P3
+//Methods needed for Linklists and game.
 
+package LinkList;
 public interface LinkList 
 {
+	//MethodName:ShuffleDeck
+	//Parameters: TBD
+	//Return: None
+	//Description: Shuffle decks!
 	public static void shuffleDeck()
 	{
 		
 	}
+	
+	//MethodName: removeCard
+	//Parameters: TBD
+	//Return: NONE
+	//Description: Remove card from Players Hand
 	public static void removeCard()
 	{
 		
 	}
 	
+	//MethodName:reviveGraveyard
+	//Parameters:TBD
+	//Return:None
+	//Description: Grab Cards from Graveyard and shuffle back into deck
+	public static void reviveGraveyard()
+	{
+		
+	}
+	
+	//MethodName: moveCardToHand()
+	//Parameters: TBD
+	//Return: NONE
+	//Description: Grab card from deck and move to players hand.
+	public static void moveCardToHand()
+	{
+		
+	}
 }
+//Problems: Figuring out what methods are needed.
